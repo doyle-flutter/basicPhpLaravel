11a8b4ac5761534e7aee17cf80a262c0c0b6dabd
doyle <il4479@hanmail.net>
2020-12-01 13:52:43 +0900

    path qs redirect

--- mylrv/routes/web.php
+++ mylrv/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+// QS
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +26,37 @@ Route::get('/data/all', function(){
 
 Route::get('/data/all/ui', function(){
     return view('hi');
+});
+
+// Params - required
+Route::get('/data/{id}', function(string $id){
+    // (1)
+    // return json_encode(['name' => 'name', 'data' => $id]);
+    // (2)
+    // return response()
+        // -> json(['name' => 'name', 'data' => $id]);
+    // (3)
+    // return response(json_encode(['name' => 'name', 'data' => $id]))
+    //     -> header('Content-Type', 'application/json');
+    // (4)
+    return response(['name' => 'name', 'data' => $id]);
+    
+});
+
+// Params - none
+Route::get('/data2/{id?}', function(string $id = "none"){
+    return response($id, 200);
+});
+
+// QS
+Route::get('/data3', function(Request $req){
+    // $url = $req->url(); // QS 없이
+    // $url = $req->fullUrl();
+    $url = $req->query('id');
+    return response($url, 200);
+});
+
+// Redirect
+Route::get('/redi', function(){
+    return redirect('/data/all');
 });
